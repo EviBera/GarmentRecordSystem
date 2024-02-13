8d5764e47920c1261bf14492c0243c24b418539b
Évi <beraevi@gmail.com>
2024-02-13 13:42:37 +0100

    Add interfaces to establish services

---
+++ GarmentRecordSystem/Services/IGarmentService.cs
@@ -0,0 +1,18 @@
+﻿using GarmentRecordSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarmentRecordSystem.Services
+{
+    public interface IGarmentService
+    {
+        void AddGarment(Garment garment);
+        void UpdateGarment(uint garmentID);
+        void DeleteGarment(uint garmentID);
+        Garment SearchGarment(uint garmentID);
+        List<Garment> SortGarments(string sortBy);
+    }
+}
---
+++ GarmentRecordSystem/Services/IJsonHandlerService.cs
@@ -0,0 +1,15 @@
+﻿using GarmentRecordSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarmentRecordSystem.Services
+{
+    public interface IJsonHandlerService
+    {
+        void SaveToFile(string filePath, List<Garment> garments);
+        List<Garment> LoadFromFile(string filePath);
+    }
+}
