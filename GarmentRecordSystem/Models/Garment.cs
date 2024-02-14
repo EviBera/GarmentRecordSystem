1d26dd03c001430eb351105405ab181d6ccd2461
Évi <beraevi@gmail.com>
2024-02-14 11:45:55 +0100

    Automate ID generation and add a constructor

--- GarmentRecordSystem/Models/Garment.cs
+++ GarmentRecordSystem/Models/Garment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,22 @@ namespace GarmentRecordSystem.Models
 {
     public class Garment
     {
-        public uint GarmentID { get; set; }
+        private static uint nextId = 1;
+        public uint GarmentID { get; init; }
         public string BrandName { get; set; }
         public DateOnly PurchaseDate { get; set; }
         public string Color { get; set; }
         public Size Size { get; set; }
+
+
+        public Garment(string brandName, DateOnly purchaseDate, string color, Size size)
+        {
+            GarmentID = nextId++;
+            BrandName = brandName;
+            PurchaseDate = purchaseDate;
+            Color = color;
+            Size = size;
+        }
+
     }
 }
