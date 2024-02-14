01fcab2e4342ed7ef6b66acc7a92ae595b45e870
Évi <beraevi@gmail.com>
2024-02-14 15:15:21 +0100

    Implement functionality to update a garment in console UI

--- ConsoleBasedUI/Program.cs
+++ ConsoleBasedUI/Program.cs
@@ -6,64 +6,70 @@ IGarmentService garmentService = new GarmentService();
 IJsonHandlerService jsonHandlerService = new JsonHandlerService();
 string filePath = "garments.json";
 
-bool exit = false;
-while (!exit)
+HandleUI();
+
+
+void HandleUI()
 {
-    Console.WriteLine("\nGarment Record System:");
-    Console.WriteLine("1. Add garment");
-    Console.WriteLine("2. Update garment");
-    Console.WriteLine("3. Delete garment");
-    Console.WriteLine("4. Search for garment");
-    Console.WriteLine("5. Sort garments");
-    Console.WriteLine("6. Save garments to file");
-    Console.WriteLine("7. Load garments from file");
-    Console.WriteLine("8. Display file content");
-    Console.WriteLine("9. Exit");
-    Console.Write("Select an option: ");
-
-    string input = Console.ReadLine();
-    int option;
-    bool isValidInput = int.TryParse(input, out option);
-
-    if (!isValidInput)
+    bool exit = false;
+    while (!exit)
     {
-        Console.WriteLine("Invalid option. Enter a number.");
-        continue;
-    }
+        Console.WriteLine("\nGarment Record System:");
+        Console.WriteLine("1. Add garment");
+        Console.WriteLine("2. Update garment");
+        Console.WriteLine("3. Delete garment");
+        Console.WriteLine("4. Search for garment");
+        Console.WriteLine("5. Sort garments");
+        Console.WriteLine("6. Save garments to file");
+        Console.WriteLine("7. Load garments from file");
+        Console.WriteLine("8. Display file content");
+        Console.WriteLine("9. Exit");
+        Console.Write("Select an option: ");
 
-    switch (option)
-    {
-        case 1:
-            AddGarment();
-            break;
-        case 2:
-            UpdateGarment();
-            break;
-        case 3:
-            DeleteGarment();
-            break;
-        case 4:
-            SearchGarment();
-            break;
-        case 5:
-            SortGarments();
-            break;
-        case 6:
-            SaveGarmentsToFile();
-            break;
-        case 7:
-            LoadGarmentsFromFile();
-            break;
-        case 8:
-            DisplayTheWholeCollection();
-            break;
-        case 9:
-            exit = true;
-            break;
+        string input = Console.ReadLine();
+        int option;
+        bool isValidInput = int.TryParse(input, out option);
+
+        if (!isValidInput)
+        {
+            Console.WriteLine("Invalid option. Enter a number.");
+            continue;
+        }
+
+        switch (option)
+        {
+            case 1:
+                AddGarment();
+                break;
+            case 2:
+                UpdateGarment();
+                break;
+            case 3:
+                DeleteGarment();
+                break;
+            case 4:
+                SearchGarment();
+                break;
+            case 5:
+                SortGarments();
+                break;
+            case 6:
+                SaveGarmentsToFile();
+                break;
+            case 7:
+                LoadGarmentsFromFile();
+                break;
+            case 8:
+                DisplayTheWholeCollection();
+                break;
+            case 9:
+                exit = true;
+                break;
         
-        default:
-            Console.WriteLine("Invalid option. Please try again.");
-            break;
+            default:
+                Console.WriteLine("Invalid option. Please try again.");
+                break;
+        }
     }
 }
 
@@ -85,40 +91,58 @@ void AddGarment()
         Console.WriteLine(ex.ToString());
     }
 
-    var garments = garmentService.GetAllGarments();
-    foreach (var garment in garments)
-    {
-        Console.WriteLine(garment.BrandName + garment.GarmentID);
-    }
-    
+    ShowMeWhatHappened();
 }
 
-static void UpdateGarment()
+void UpdateGarment()
 {
-    Console.WriteLine("UpdateGarment method");
+    Console.WriteLine("Enter the ID of the garment that needs an update: ");
+    var idInput = Console.ReadLine();
+    uint id;
+
+    if (!uint.TryParse(idInput, out id))
+    {
+        Console.WriteLine("Invalid ID format.");
+        return;
+    }
+    Garment? oldGarment = garmentService.SearchGarment(id);
+    if(oldGarment == null)
+    {
+        Console.WriteLine("Invalid ID, garment not found.");
+        return;
+    }
+
+    var newBrandName = UpdateBrandName(oldGarment);
+    var newColor = UpdateColor(oldGarment);
+    var newPurchase = UpdatePurchase(oldGarment);
+    var newSize = UpdateSize(oldGarment);
+
+    garmentService.UpdateGarment(id, newBrandName, newPurchase, newColor, newSize);
+
+    ShowMeWhatHappened();
 }
 
-static void DeleteGarment()
+void DeleteGarment()
 {
     Console.WriteLine("DeleteGarment method");
 }
 
-static void SearchGarment()
+void SearchGarment()
 {
     Console.WriteLine("SearchGarment method");
 }
 
-static void SortGarments()
+void SortGarments()
 {
     Console.WriteLine("SortGarment method");
 }
 
-static void SaveGarmentsToFile()
+void SaveGarmentsToFile()
 {
     Console.WriteLine("Garments saved to file successfully.");
 }
 
-static void LoadGarmentsFromFile()
+void LoadGarmentsFromFile()
 {
     Console.WriteLine("Garments loaded from file successfully.");
 }
@@ -233,4 +257,95 @@ Size RegisterSize()
     }
     return size;
 
+}
+
+void ShowMeWhatHappened()
+{
+    var garments = garmentService.GetAllGarments();
+    foreach (var garment in garments)
+    {
+        Console.WriteLine(garment.BrandName + " " + garment.GarmentID);
+    }
+
+}
+
+string UpdateBrandName(Garment oldGarment){
+    Console.WriteLine("Current brand name is: " + oldGarment.BrandName);
+    bool isValid = false;
+    string newBrandName = oldGarment.BrandName;
+    while (!isValid)
+    {
+        Console.WriteLine("Would you like to change it (y / n)?");
+        var answer = Console.ReadLine();
+        if (answer.ToLower() == "n")
+        {
+            isValid = true;
+        }
+        if (answer.ToLower() == "y")
+        {
+            newBrandName = RegisterString("Brand name");
+            isValid = true;
+        }
+    }
+    return newBrandName;
+}
+string UpdateColor(Garment oldGarment){
+    Console.WriteLine("Current color is: " + oldGarment.Color);
+    bool isValid = false;
+    string newColor = oldGarment.Color;
+    while (!isValid)
+    {
+        Console.WriteLine("Would you like to change it (y / n)?");
+        var answer = Console.ReadLine();
+        if (answer.ToLower() == "n")
+        {
+            isValid = true;
+        }
+        if (answer.ToLower() == "y")
+        {
+            newColor = RegisterString("Color");
+            isValid = true;
+        }
+    }
+    return newColor;
+}
+DateOnly UpdatePurchase(Garment oldGarment){
+    Console.WriteLine("Date of purchase is: " + oldGarment.PurchaseDate);
+    bool isValid = false;
+    DateOnly newPurchase = oldGarment.PurchaseDate;
+    while (!isValid)
+    {
+        Console.WriteLine("Would you like to change it (y / n)?");
+        var answer = Console.ReadLine();
+        if (answer.ToLower() == "n")
+        {
+            isValid = true;
+        }
+        if (answer.ToLower() == "y")
+        {
+            newPurchase = RegisterPurchaseDate();
+            isValid = true;
+        }
+    }
+    return newPurchase;
+}
+Size UpdateSize(Garment oldGarment){
+    Console.WriteLine("Currest size is: " + oldGarment.Size);
+    bool isValid = false;
+    Size newSize = oldGarment.Size;
+    while (!isValid)
+    {
+        Console.WriteLine("Would you like to change it (y / n)?");
+        var answer = Console.ReadLine();
+        if (answer.ToLower() == "n")
+        {
+            isValid = true;
+        }
+        if (answer.ToLower() == "y")
+        {
+            newSize = RegisterSize();
+            isValid = true;
+        }
+    }
+    return newSize;
 }
